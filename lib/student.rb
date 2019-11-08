3205f145a7037e329f01cc7399969bf98fab50e4
Naya  Willis <greedybrain18@gmail.com>
2019-11-08 20:29:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ class Student
   end
   
   def self.create(attributes)
-    attributes.each {|key, value| self.send(self.new("#{k}", v))}
+    attributes.each {|key, value| self.send(self.new("#{key}", value))}
   end
   
 end
