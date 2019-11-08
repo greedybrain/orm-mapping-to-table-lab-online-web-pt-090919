179df6751c9bdb2e7cd7056015c0a0d3f9d47b78
Naya  Willis <greedybrain18@gmail.com>
2019-11-08 20:32:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten[0]
   end
   
-  def self.create(attributes)
+  def self.create(name:, grade:)
     student = Student.new()
   end
   
